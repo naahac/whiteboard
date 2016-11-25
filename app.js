1cb3b58233e90e390df453152ae7dffec838ceb0
Natanael Ahac <natanael.ahac@gmail.com>
2016-11-26 00:27:07 +0100

    commit

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var http = require('http');
 var app = express();
-app.set('port', (process.env.PORT || 5000));
-console.log(app.get('port'))
-var server = http.createServer(app)
+var port = process.env.PORT || 8000;
+console.log(port)
+var server = http.createServer(app).listen(port)
 var io = require('socket.io').listen(server);
-//server.listen(process.env.PORT || 5000);
-
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
