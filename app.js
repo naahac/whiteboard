5885b45264c47a2910d1cd56ddcd6ce7bd9adcd8
Natanael Ahac <natanael.ahac@gmail.com>
2016-11-25 23:57:30 +0100

    commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 console.log(app.get('port'))
 var server = http.createServer(app)
 var io = require('socket.io').listen(server);
-server.listen(process.env.PORT || 5000);
+//server.listen(process.env.PORT || 5000);
 
 var path = require('path');
 var favicon = require('serve-favicon');
